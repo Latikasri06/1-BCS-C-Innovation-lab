193f41e4921e89b870a5984643dbd50becdb7fb9
Latika sri S <latikasri2007@gmail.com>
2025-07-21 22:23:47 +0530

    Create Day2_Task3.js

---
+++ Day2_Task3.js
@@ -0,0 +1,28 @@
+for(let i=2;i<=20;i++) 
+    {
+        let isPrime = true;
+        for (let j=2;j<i;j++) 
+            {
+                if (i%j===0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+            } 
+            if(isPrime) 
+                {
+                    console.log("Prime numbers:",i);
+                    break;
+                }
+    }
+
+
+
+for (let i = 1; i <= 20; i++) 
+    {
+        if (i%2===0) 
+            {
+                continue;
+            }
+            console.log(i);
+    }
