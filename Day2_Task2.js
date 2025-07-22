b81fdc8dc38a27ee0b2842a0fb6e4f829ba8c0aa
Latika sri S <latikasri2007@gmail.com>
2025-07-22 07:55:45 +0530

    Update Day2_Task2.js

--- Day2_Task2.js
+++ Day2_Task2.js
@@ -1,9 +1,13 @@
+// while loop
+
 let i = 1;
 while (i <= 10) 
     {
         console.log(i);
         i++;
     }
+
+// do-while loop
 
 let input;
 do 
@@ -12,6 +16,7 @@ do
 } 
 while (input <= 10);
 
+// for loop
 
 let n = 5;
 let sum = 0;
@@ -21,6 +26,7 @@ for (let i=1;i<=n;i++)
     }
         console.log("Sum:", sum);
 
+// nested for loop
 
 for (let i = 1; i <= 5; i++) 
     {
